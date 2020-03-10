90e4205c0db9b069c609e55e8c45b1aae4788447
g.ishiguro <genki.i0918@gmail.com>
2020-03-10 22:45:39 +0900

    有給api実装

---
+++ app/Http/Controllers/UqapiController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Yasumi\Yasumi;
+
+class UqapiController extends Controller
+{
+    public function index(Request $request) {
+        if ($request->input('year')) {
+            $year = $request->input('year');
+        } else {
+            // 指定がない場合は今年の祝日を取得します。
+            $year =  date('Y');
+        }
+
+        $arr = array();
+        $holidays = Yasumi::create('Japan', $year, 'ja_JP');
+        $count = 0;
+        foreach($holidays as $key => $holiday) {
+            $arr[$count]['date'] = $holiday->format('Y-m-d');
+            $arr[$count]['name'] = $holiday->getName();
+
+            $count++;
+        }
+        return $arr;
+    }
+}
